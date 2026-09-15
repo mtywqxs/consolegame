d518aec61f60160d68f9a5346f596decff4d2097
username <mail@ex.com>
2026-09-15 16:47:05 +0500

    + 2 modes

--- minigame.py
+++ minigame.py
@@ -15,8 +15,9 @@ class gameofnumbers:
         self.game_flag = True
         self.mistake_flag = False
         self.answer_list = []
-        self.answer = (0, 0)  # (число в системе, десятичное значение)
+        self.answer = (0, 0)
         self.commands = ['выход', 'статистика'] 
+        self.mode = 0 # 1 = from 10; 2 = to 10
 
     def cmd_clean(self):
         system('cls' if name == 'nt' else 'clear')
@@ -33,7 +34,7 @@ class gameofnumbers:
             try:
                 print("\nДоступно 3 уровня сложности :\n  1) числа от 0 до 15.\n  2) числа от 0 до 256.\n  3) числа от 0 до 1023 \n")
                 difficulty_inp = int(input("Выберите уровень сложности : \n>>  "))
-                if difficulty_inp > 0 and difficulty_inp < 4:
+                if difficulty_inp in (1,2,3):
                     self.difficulty = difficulty_inp
                     print(f"Отлично! Уровень сложности : {self.difficulty}.\n")
                     self.set_difficulty_parameters()
@@ -72,17 +73,35 @@ class gameofnumbers:
                 input("Нажмите любую клавишу для продолжения . . . ")
                 self.cmd_clean()
 
+    def getmode(self):
+        while 1:
+            try:
+                print(f"\nДоступно 2 режима :\n 1) Из 10 системы исчисления в {self.system} \n 2) Из {self.system} системы в 10 \n")
+                choice = int(input("Выберите режим (ввести только число): \n>>  "))
+                if choice in (1,2):
+                    self.mode = choice
+                    print("Выбран ", self.mode, " режим.\n")
+                    return
+                else:
+                    raise Exception
+            except:
+                print("Выберите систему (2, 8, 16).")
+            finally:
+                input("Нажмите любую клавишу для продолжения . . . ")
+                self.cmd_clean()
+
     def show_statistic(self):
         self.cmd_clean()
-        print("Ваша игровая статистика :")
+        print("\nВаша игровая статистика :")
         print(f"Баллов : {self.points}; Раундов сыгранно : {self.rounds}")
-        print(f"Oшибок совершенно : {self.mistakes}; Система исчисления : {self.system}\n\n")
+        print(f"Oшибок совершенно : {self.mistakes}; Система исчисления : {self.system}")
+        mode_str = f"Из 10 системы исчисления в {self.system}" if self.mode == 1 else f"Из {self.system} системы в 10"
+        print(f"Режим : {mode_str}\n\n")
         print("Нажмите любую клавишу для выхода . . . ")
 
     def byebye(self):
-        print("До новых встреч!")
         self.show_statistic()
-        input()
+        input("\n\t\tДо встречи!")
         exit()
         
     def generate_count(self):
@@ -95,15 +114,21 @@ class gameofnumbers:
             self.answer =( hex(count)[2::], count )
 
     def make_answers(self):
-        self.answer_list = [self.answer[1]]
+        self.answer_list = [self.answer[1]] if self.mode == 2 else [self.answer[0]]
         for _ in range(4):
             temp_answer = randint(self.start_point, self.end_point)
+            if self.mode == 1:
+                if self.system == 2:
+                    temp_answer = bin(temp_answer)[2::]
+                elif self.system == 8:
+                    temp_answer = oct(temp_answer)[2::]
+                elif self.system == 16:
+                    temp_answer = hex(temp_answer)[2::]
             self.answer_list.append(temp_answer)
         shuffle(self.answer_list)
-        
 
     def show_answers(self):
-        print(f"\nВаше число : {self.answer[0]}({self.system}).")
+        print(f"\nВаше число : {self.answer[0] if self.mode == 2 else self.answer[1]}({self.system if self.mode == 2 else 10}).")
         print(f"Варианты ответов :")
 
         ansswers = "  "
@@ -113,7 +138,14 @@ class gameofnumbers:
 
 
     def check_answer(self, user_answer : str):
-        if user_answer == str(self.answer[1]):
+        user_answer = user_answer.strip()
+        correct_answer = (str(self.answer[1]) if self.mode == 2 else str(self.answer[0]))
+
+        if self.mode == 1 and self.system == 16:
+            user_answer = user_answer.upper()
+            correct_answer = correct_answer.upper()
+
+        if user_answer == correct_answer:
             self.mistake_flag = False
             self.points += 1
             print(f"Отлично! Вы дали верный ответ за {self.attempts} попыток. Балл засчитан.")
@@ -141,10 +173,12 @@ game = gameofnumbers()
 game.greeting()
 game.getdifficulty()
 game.getsystem()
+game.getmode()
 while game.game_flag:
     if game.mistake_flag is False:
         game.generate_count()
         game.make_answers()
     game.show_answers()
+    print(game.answer)
     user_input = input("\n>>  ")
     game.check_answer(user_input)
